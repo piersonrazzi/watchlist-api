ed67d5decbe8bf45c9a86399155b2728fba9b5b9
Pierson Razzi <razzi.pierson@gmail.com>
2026-09-23 11:44:49 -0400

    Initial Spring Boot project from start.spring.io (Maven wrapper, Web, JPA, H2, Validation)

---
+++ src/test/java/dev/razzi/watchlist/WatchlistApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.razzi.watchlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchlistApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
